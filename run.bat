7120da793674dbce89e85ce71a57e7244a5e87cc
Long Nguyen <nguyen.long.908132@gmail.com>
2022-10-07 23:01:14 +0700

    Add warnings about using Windows <10

--- run.bat
+++ run.bat
@@ -1,5 +1,12 @@
 @echo off
-chcp 65001 >nul
+REM Windows version detection
+for /f "tokens=4-7 delims=[.] " %%i in ('ver') do (if %%i==Version (set v=%%j.%%k) else (set v=%%i.%%j))
+if "%v%" == "10.0" (
+    chcp 65001 >nul
+) else (
+    @echo You're not using Windows 10/11. Vietnamese text will be broken
+    pause
+)
 where /q pwsh
 if not ERRORLEVEL 0 (
     @echo Không tìm thấy PowerShell. Script này yêu cầu PowerShell 7 trở lên
