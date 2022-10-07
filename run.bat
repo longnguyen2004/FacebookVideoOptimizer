03037facdc17a14c40d76d6dac47375cc91accfb
Long Nguyen <nguyen.long.908132@gmail.com>
2022-10-07 17:17:13 +0700

    Check for PowerShell on startup

--- run.bat
+++ run.bat
@@ -1,2 +1,9 @@
 @echo off
+chcp 65001 >nul
+where /q pwsh
+if not ERRORLEVEL 0 (
+    @echo Không tìm thấy PowerShell. Script này yêu cầu PowerShell 7 trở lên
+    @echo Vui lòng truy cập https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows#msi
+    pause && exit
+)
 pwsh -ExecutionPolicy Bypass -File Main.ps1
