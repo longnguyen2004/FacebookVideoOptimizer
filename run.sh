d74a1ad8d63c18b83dacadda6435044f64eefa3c
Long Nguyen <nguyen.long.908132@gmail.com>
2022-10-18 11:47:02 +0700

    Fix syntax errors

--- run.sh
+++ run.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-if ! command -v pwsh &> /dev/null
-    echo Không tìm thấy PowerShell. Script này yêu cầu PowerShell 7 trở lên
+if ! command -v pwsh >/dev/null 2>&1; then
+    echo "Không tìm thấy PowerShell. Script này yêu cầu PowerShell 7 trở lên"
     uname="$(uname -s)"
     case "${uname}" in
-        Linux*)  OS=linux
-        Darwin*) OS=macos
-    echo Vui lòng truy cập https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-$OS
-    read -p "Press any key to continue... " -n1 -s
+        Linux*)  OS=linux;;
+        Darwin*) OS=macos;;
+    esac
+    echo "Vui lòng truy cập https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-$OS"
+    echo "Press Enter to exit"
+    read _
     exit
 fi
 pwsh -File Main.ps1 "$@"
