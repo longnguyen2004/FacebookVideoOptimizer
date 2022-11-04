e3c6de08d28ed56c89c4137d81b6712f3b3defd7
Long Nguyen <nguyen.long.908132@gmail.com>
2022-11-04 19:28:20 +0700

    Rearrange flags + process audio separately

--- script/Encode.ps1
+++ script/Encode.ps1
@@ -1,6 +1,6 @@
 . $PSScriptRoot/Get-Choice.ps1;
 
-function Encode {
+function Encode-Video {
     param(
         [string] $InputFile,
         [string] $OutputFile
@@ -15,6 +15,7 @@ Chọn loại encoder:
 "@
     $EncoderType = [int](Get-Choice -Choices 1, 2 -Default 1);
     Write-Host;
+
     switch ($EncoderType)
     {
         1 {
@@ -27,17 +28,20 @@ Chọn loại encoder:
 
     $Encoder = $EncoderSettings.Encoder;
     $CommonParam = $EncoderSettings.CommonParam;
-    Write-Host "Bitrate: ${VideoBitrate}kbps (video) + ${AudioBitrate}kbps (audio)";
+    Write-Host;
+    Write-Host "Đang xử lý video...";
+    Write-Host "Bitrate: ${VideoBitrate}kbps";
     Write-Host "Cài đặt encoder: $CommonParam`n";
 
     if ($EncoderSettings."2PassParam")
     {
         $Pass1Param, $Pass2Param = $EncoderSettings."2PassParam";
         Write-Host "Pass 1:";
-        & "$FFmpeg" -y -i "$InputFile" `
-            -hide_banner -loglevel $LogLevel -stats -fps_mode cfr `
-            -c:v $Encoder -b:v "${VideoBitrate}k"                 `
-            @CommonParam @Pass1Param                              `
+        & "$FFmpeg" `
+            -hide_banner -loglevel $LogLevel -stats `
+            -y -i "$InputFile" -fps_mode cfr        `
+            -c:v $Encoder -b:v "${VideoBitrate}k"   `
+            @CommonParam @Pass1Param                `
             -an -f null ($IsWindows ? "NUL" : "/dev/null")
         Write-Host;
         if ($LASTEXITCODE -ne 0) 
@@ -46,12 +50,12 @@ Chọn loại encoder:
         }
 
         Write-Host "Pass 2:"
-        & "$FFmpeg" -y -i "$InputFile" `
-            -hide_banner -loglevel $LogLevel -stats -fps_mode cfr `
-            -c:v $Encoder -b:v "${VideoBitrate}k"                 `
-            @CommonParam @Pass2Param                              `
-            -movflags +faststart                                  `
-            "$OutputFile"
+        & "$FFmpeg" `
+            -hide_banner -loglevel $LogLevel -stats `
+            -y -i "$InputFile" -fps_mode cfr        `
+            -c:v $Encoder -b:v "${VideoBitrate}k"   `
+            @CommonParam @Pass2Param                `
+            -an "$OutputFile"
         Write-Host;
         Remove-Item "*2pass*";
         if ($LASTEXITCODE -ne 0) 
@@ -61,12 +65,12 @@ Chọn loại encoder:
     }
     else
     {
-        & "$FFmpeg" -y -i "$InputFile" `
-            -hide_banner -loglevel $LogLevel -stats -fps_mode cfr `
-            -c:v $Encoder -b:v "${VideoBitrate}k"                 `
-            @CommonParam                                          `
-            -movflags +faststart                                  `
-            "$OutputFile"
+        & "$FFmpeg" `
+            -hide_banner -loglevel $LogLevel -stats `
+            -y -i "$InputFile" -fps_mode cfr        `
+            -c:v $Encoder -b:v "${VideoBitrate}k"   `
+            @CommonParam                            `
+            -an "$OutputFile"
         Write-Host;
         if ($LASTEXITCODE -ne 0) 
         {
@@ -74,4 +78,47 @@ Chọn loại encoder:
         }
     }
     return $true;
+}
+
+function Encode-Audio {
+    param(
+        [string] $InputFile,
+        [string] $OutputFile
+    )
+    Write-Host "Đang xử lý audio...";
+    Write-Host "Bitrate: ${AudioBitrate}kbps";
+    & "$FFmpeg" -y -i "$InputFile" `
+        -hide_banner -loglevel $LogLevel -stats `
+        -c:a aac -b:a "${AudioBitrate}k"        `
+        -af "lowpass=f=16000:r=f64"             `
+        -vn "$OutputFile"
+    Write-Host;
+    return ($LASTEXITCODE -eq 0);
+}
+function Encode {
+    param(
+        [string] $InputFile,
+        [string] $OutputFile
+    )
+    $TempDir = $IsWindows ? $Env:Temp : "/tmp";
+    $VideoFile = Join-Path "$TempDir" "video.mp4";
+    $AudioFile = Join-Path "$TempDir" "audio.m4a";
+
+    $VideoResult = Encode-Video $InputFile $VideoFile;
+    if (-not $VideoResult) { return $false };
+
+    $AudioResult = Encode-Audio $InputFile $AudioFile;
+    if (-not $AudioResult) { return $false };
+
+    Write-Host "Muxing...";
+    & "$FFmpeg" `
+        -y -hide_banner -loglevel $LogLevel -stats `
+        -i "$VideoFile" -i "$AudioFile"            `
+        -c copy "$OutputFile"
+    Write-Host;
+
+    Remove-Item $VideoFile -Force;
+    Remove-Item $AudioFile -Force;
+
+    return $LASTEXITCODE -eq 0;
 }
