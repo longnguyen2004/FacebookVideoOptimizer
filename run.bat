f53737c980b7d85f0fb8b06c0aced6b3728c3a4c
Long Nguyen <nguyen.long.908132@gmail.com>
2022-11-11 07:34:59 +0700

    Remove chcp exception + add more instructions

--- run.bat
+++ run.bat
@@ -2,14 +2,15 @@
 setlocal
 REM Windows version detection
 for /f "tokens=4-7 delims=[.] " %%i in ('ver') do (if %%i==Version (set v=%%j.%%k) else (set v=%%i.%%j))
-if "%v%" == "10.0" (
-    chcp 65001 >nul
-) else (
-    @echo You're not using Windows 10/11. Vietnamese text will be broken
+if "%v%" NEQ "10.0" (
+    @echo You're not using Windows 10/11. You need to change the font to prevent crashes
+    @echo See the README for more info
+    @echo Please change the font before continuing
     pause
 )
+chcp 65001 >nul
 where /q pwsh
-if not %ERRORLEVEL% == 0 (
+if %ERRORLEVEL% NEQ 0 (
     @echo Không tìm thấy PowerShell. Script này yêu cầu PowerShell 7 trở lên
     @echo Vui lòng truy cập https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows#msi
     pause && exit
