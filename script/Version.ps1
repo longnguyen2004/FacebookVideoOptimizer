7e3c92b990b94787db6b595de53e80af7159f1c5
Long Nguyen <nguyen.long.908132@gmail.com>
2023-04-16 23:04:44 +0700

    Make updating code more deterministic (hopefully)

--- script/Version.ps1
+++ script/Version.ps1
@@ -9,19 +9,19 @@ $ProgressPreference = "SilentlyContinue";
 
 if ((Test-NetConnection))
 {
-    $LatestVersion = (Invoke-RestMethod "https://api.github.com/repos/longnguyen2004/FacebookVideoOptimizer/tags")[0];
-    if ([System.Version]$LatestVersion.name -gt [System.Version]$FullVersion)
+    $LatestVersion = (Invoke-RestMethod "https://api.github.com/repos/longnguyen2004/FacebookVideoOptimizer/tags")[0].name;
+    if ([System.Version]$LatestVersion -gt [System.Version]$FullVersion)
     {
-        Write-Host ($Strings["UpdateAvailable"] -f $LatestVersion.name);
+        Write-Host ($Strings["UpdateAvailable"] -f $LatestVersion);
         if ((Get-UserInput -Choices "Y","N") -eq "Y")
         {
             $TempPath = [System.IO.Path]::GetTempPath();
             $ZipPath = Join-Path "$TempPath" "update.zip";
-            Invoke-WebRequest $LatestVersion.zipball_url -OutFile "$ZipPath";
+            Invoke-WebRequest "https://github.com/longnguyen2004/FacebookVideoOptimizer/archive/refs/tags/${LatestVersion}.zip" -OutFile "$ZipPath";
             Expand-Archive -LiteralPath "$ZipPath" -DestinationPath "$TempPath";
-            $Folder = Join-Path "$TempPath" "longnguyen2004-FacebookVideoOptimizer-*";
-            Get-ChildItem "$PSScriptRoot/../*" -Exclude "tools" | Remove-Item -Recurse -Force;
-            Move-Item (Join-Path "$Folder" "*") -Destination "$PSScriptRoot/..";
+            $Folder = Join-Path "$TempPath" "FacebookVideoOptimizer-$LatestVersion";
+            Get-ChildItem "$RootDir/*" -Exclude "tools" | Remove-Item -Recurse -Force;
+            Move-Item (Join-Path "$Folder" "*") -Destination "$RootDir";
             Remove-Item -Recurse -Force "$Folder";
             Remove-Item -Force "$ZipPath";
             Write-Host $Strings["UpdateFinished"];
